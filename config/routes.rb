542eff28a0835ba2240a52f9920f64a2caf4dffd
sunflower-field <himartmusume@gmail.com>
2022-10-05 13:47:32 +0000

    [Finish]8-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
-
-  # resources :post_images, only: [:new, :index, :show]
-
+  root to: 'homes#top' 
   get 'homes/about' => 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
