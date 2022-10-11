5ecfe35efc7df06216c040b95baf9f335e5a93fe
sunflower-field <himartmusume@gmail.com>
2022-10-11 14:22:49 +0000

    [middle]8-18Controllerを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   has_one_attached :profile_image
 
@@ -13,5 +14,5 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
-  end
+   end
 end
